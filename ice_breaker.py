3149e229d3e9fc5c3c907fdd0806830d3c724da9
Shubham Kumar <shubh199815@gmail.com>
2023-06-25 11:57:18 +0530

    Added api and basic frontend

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,10 @@
+from typing import Tuple
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
 
 from dotenv import load_dotenv
@@ -12,9 +14,9 @@ from third_parties.twitter import scrape_user_tweets
 load_dotenv()
 
 name = "Harrison Chase"
-if __name__ == "__main__":
-    print("Hello Langchain!")
 
+
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -27,9 +29,12 @@ if __name__ == "__main__":
         2. two interesting facts about them
         3. A topic that may interest them
         4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["linkedin", "twitter"], template=summary_template
+        input_variables=["linkedin", "twitter"],
+        template=summary_template,
+        partial_variables={"format_instructions":person_intel_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(
@@ -38,4 +43,12 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin=linkedin_data, twitter=tweets))
+    result = chain.run(linkedin=linkedin_data, twitter=tweets)
+    print(result)
+
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    print("Hello Langchain!")
+    ice_break(name="Harrison Chase")
